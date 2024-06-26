93285d7f666b0cb61c6711f8c97610973babf314
摇曳的枝桠 <116493724+HazBegonia@users.noreply.github.com>
2024-06-26 11:41:12 +0800

    Add files via upload

---
+++ V/Django/TR/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Form, User, appuser
+
+admin.site.register(Form)
+admin.site.register(User)
+admin.site.register(appuser)
+
+
+
+# Register your models here.
